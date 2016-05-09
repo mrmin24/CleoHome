19a1f681db8e4e3b45cbe2458f9992c7fb400ffd
mrmin24 <mariusminny@gmail.com>
2016-05-09 22:39:17 +0200

    rules fixed maybe

--- JSModules/Rule_Items_Evaluate.js
+++ JSModules/Rule_Items_Evaluate.js
@@ -13,25 +13,36 @@ exports.evaluateChange = function(itemId,itemValue,callback){
        if(err){
            
            myconsole.log(err);
-       }else if(result ){
+       }else if(result){
            
           // myconsole.log(result);
-           var status = 0;
+         
            
            for(var i in result){
-               if(result[i].Equals == itemValue  && result[i].Equals != null){
+               
+               
+               checkStatus(result[i]);
+               
+               
+           }
+           
+               
+               
+             function checkStatus(results){  
+                 var status = 0;
+               if(results.Equals == itemValue  && results.Equals != null){
                    status = 1;
-               }else if(itemValue > result[i].Greater_Than  && result[i].Greater_Than != null){
+               }else if(itemValue > results.Greater_Than  && results.Greater_Than != null){
                    status = 1;
-               }else if(itemValue < result[i].Less_Than && result[i].Less_Than != null){
+               }else if(itemValue < results.Less_Than && results.Less_Than != null){
                    status = 1;
-               }else if(result[i].Not_Equal != itemValue  && result[i].Not_Equal != null){
+               }else if(results.Not_Equal != itemValue  && results.Not_Equal != null){
                    status = 1;
                }
                
                if(status == 1 ){
                    var rulechanged = 0;
-                   data = {'Set':'Status','Where':'Second_Id','Current_State':1,'Name':result[i].Second_Id};
+                   data = {'Set':'Status','Where':'Second_Id','Current_State':1,'Name':results.Second_Id};
                 
                    db.update('Rule_Items',data,function(err,result2){
                        
@@ -48,14 +59,14 @@ exports.evaluateChange = function(itemId,itemValue,callback){
                        }
                        
                  
-                   
+                  
                 
-                        var rules = result[i].Rule_Id.split(';');     //determine which rules fit with this ID
+                        var rules = results.Rule_Id.split(';');     //determine which rules fit with this ID
                         
                   
                    var ids = "";
                   // myconsole.log(rules);
-                   for(var j = 0; j < (rules.length * !result[i].Secondary_Item * rulechanged) ; j++){
+                   for(var j = 0; j < (rules.length * !results.Secondary_Item * rulechanged) ; j++){
                        if(!isNaN(rules[j])){
                            var rulenr = rules[j];
                           // myconsole.log(rules[i]);
@@ -128,8 +139,8 @@ exports.evaluateChange = function(itemId,itemValue,callback){
                    status = 0;
                     callback(null,null,null,null,null,null);
                }
-           }
-          
+           
+             }
        }else{
             callback(null,null,null,null,null,null);
        }
